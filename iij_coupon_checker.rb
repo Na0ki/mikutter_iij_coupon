2b9e38a799acfa8f4d1498f678fd1cc50e70aab1
ahiru <wellingtonnmaeda0524@gmail.com>
2016-12-25 17:41:55 +0900

    黒魔術練成中

---
+++ iij_coupon_checker.rb
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+require 'httpclient'
+require 'uri'
+require 'json'
+
+require_relative 'model'
+
+Plugin.create(:iij_coupon_checker) do
+
+  class IDNotFoundError < StandardError; end
+
+  begin
+    id = UserConfig['iij_developer_id']
+    if id.nil? || id == ''
+      raise IDNotFoundError
+    else
+      @client_id = id
+    end
+  rescue IDNotFoundError => err
+    activity :iij_coupon_checker, "デベロッパーIDが存在しません\nIDを設定してください\n: #{err}"
+    error err
+  end
+
+
+  def auth
+    uri = URI.parse('https://api.iijmio.jp/mobile/d/v1/authorization/')
+    query = {
+        :response_type => 'token',
+        :client_id => @client_id,
+        :state => 'mikutter_iij_coupon_checker',
+        :redirect_uri => UserConfig['iij_redirect_uri'] || 'localhost'
+    }.to_hash
+    uri.query = URI.encode_www_form(query)
+
+    Thread.new {
+      Plugin.call(:open, uri)
+    }.next { |response|
+      Delayer::Deferred.fail(response) unless (response.nil? or response&.status == 200)
+      # puts response
+      # p response.status
+      # p response.contenttype
+      # p response.body
+      p response
+    }.trap { |err|
+      activity :iij_coupon_checker, "認証に失敗しました: #{err.to_s}"
+      error err
+    }
+  end
+
+
+  def check_coupon
+    Thread.new {
+
+    }.next { |_|
+
+    }.trap { |err|
+      activity :iij_coupon_checker, "クーポン情報の取得に失敗しました: #{err}"
+      error err
+    }
+  end
+
+
+  command(:check_iij_coupon,
+          name: 'クーポンの確認をする',
+          condition: lambda { |_| true },
+          visible: true,
+          role: :timeline
+  ) do |_|
+    auth
+  end
+
+
+  # mikutter設定画面
+  # @see http://mikutter.blogspot.jp/2012/12/blog-post.html
+  settings('iijクーポン') do
+    settings('デベロッパID') do
+      input 'デベロッパID', :iij_developer_id
+    end
+
+    settings('リダイレクトURI') do
+      input 'URI', :iij_redirect_uri
+    end
+  end
+
+end
