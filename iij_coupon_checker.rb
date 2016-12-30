b19913b18e0609e6053c153b51d75769d5940a85
ahiru <wellingtonnmaeda0524@gmail.com>
2016-12-30 17:05:34 +0900

    無駄なメソッドを排除

--- iij_coupon_checker.rb
+++ iij_coupon_checker.rb
@@ -1,52 +1,10 @@
 # -*- coding: utf-8 -*-
 
-require 'httpclient'
-require 'uri'
 require 'json'
 
 require_relative 'model'
 
 Plugin.create(:iij_coupon_checker) do
-
-  # クーポンの取得
-  def check_coupon
-    Plugin::IIJ_COUPON_CHECKER::CouponInfo.get_info.next { |data|
-      data.each { |d|
-        info = d.instance_variable_get(:@value)
-        msg = "hdoServiceCode: #{info[:hdo_info].hdoServiceCode}\n" +
-            "電話番号: #{info[:hdo_info].number}\n" +
-            "クーポン利用状況: #{info[:hdo_info].couponUse ? '使用中' : '未使用'}\n" +
-            "規制状態: #{info[:hdo_info].regulation ? '規制中' : '規制なし'}\n" +
-            "SIM内クーポン残量: #{info[:hdo_info].coupon.volume} [MB]"
-        # 投稿
-        post(msg)
-      }
-    }.trap { |err|
-      activity :iij_coupon_checker, "クーポン情報の取得に失敗しました: #{err}"
-      error err
-    }
-  end
-
-
-  # クーポンのオン・オフの切り替え
-  # @param [String] hdo hdoServiceInfo
-  # @param [boolean] is_valid オン/オフ
-  def switch_coupon(hdo, is_valid)
-    Plugin::IIJ_COUPON_CHECKER::CouponInfo.switch(hdo, is_valid).next { |_|
-      msg = "クーポンのステータスが変更されました\n" +
-          "hdoServiceCode: #{hdo}\n" +
-          "現在の状態: #{is_valid ? '使用中' : '未使用'}"
-      # 投稿
-      post(msg)
-    }.trap { |err|
-      activity :iij_coupon_checker, "クーポンの切り替えに失敗しました: #{err}"
-      error err
-      msg = "ステータスコード: #{err.status} (#{err.reason})\n" +
-          "詳細: #{JSON.parse(err.content).dig('returnCode')}"
-      post(msg)
-    }
-  end
-
 
   # メッセージの投稿
   # @param [String] msg 投稿メッセージ
@@ -64,11 +22,24 @@ Plugin.create(:iij_coupon_checker) do
           name: 'クーポンの確認をする',
           condition: lambda { |_| true },
           visible: true,
-          role: :timeline
-  ) do |_|
+          role: :timeline) { |_|
     # クーポンの取得
-    check_coupon
-  end
+    Plugin::IIJ_COUPON_CHECKER::CouponInfo.get_info.next { |data|
+      data.each { |d|
+        info = d.instance_variable_get(:@value)
+        msg = "hdoServiceCode: #{info[:hdo_info].hdoServiceCode}\n" +
+            "電話番号: #{info[:hdo_info].number}\n" +
+            "クーポン利用状況: #{info[:hdo_info].couponUse ? '使用中' : '未使用'}\n" +
+            "規制状態: #{info[:hdo_info].regulation ? '規制中' : '規制なし'}\n" +
+            "SIM内クーポン残量: #{info[:hdo_info].coupon.volume} [MB]"
+        # 投稿
+        post(msg)
+      }
+    }.trap { |err|
+      activity :iij_coupon_checker, "クーポン情報の取得に失敗しました: #{err}"
+      error err
+    }
+  }
 
 
   # クーポンの利用オン・オフ切り替え
@@ -76,11 +47,24 @@ Plugin.create(:iij_coupon_checker) do
           name: 'クーポンの使用を変更',
           condition: lambda { |_| true },
           visible: true,
-          role: :timeline
-  ) do |_|
+          role: :timeline) { |_|
     # TODO: オンオフするhdoを選択できるようにする
-    switch_coupon('YOUR_HDO_CODE_HERE', true)
-  end
+    hdo = 'YOUR_HDO_CODE_HERE'
+    is_valid = true
+    Plugin::IIJ_COUPON_CHECKER::CouponInfo.switch(hdo, is_valid).next { |_response|
+      msg = "クーポンのステータスが更新されました\n" +
+          "hdoServiceCode: #{hdo}\n" +
+          "クーポンのステータス: #{is_valid ? 'オン' : 'オフ'}"
+      # 投稿
+      post(msg)
+    }.trap { |err|
+      activity :iij_coupon_checker, "クーポンの切り替えに失敗しました: #{err}"
+      error err
+      msg = "ステータスコード: #{err.status} (#{err.reason})\n" +
+          "詳細: #{JSON.parse(err.content).dig('returnCode')}"
+      post(msg)
+    }
+  }
 
 
   # アクティビティの設定
